4397de950060586cf54b8c3a91f02a2cbd6d8e1d
Nicolas de Paula Pacheco <nick.paulo.pacheco@gmail.com>
2024-06-27 13:52:58 -0300

    Alterações

--- src/views/fetch.js
+++ src/views/fetch.js
@@ -1,27 +1,18 @@
-/**}
- * Assincronismo
- * fetch (promise simplificada)
- * @author nicolas de Paula Pacheco
- */
+function pegarCep() {
+    let cep = (frmFornecedor.inputCep.value)
+    let urlAPI = `https://viacep.com.br/ws/${cep}/json/`
 
-const read = require('readline-sync')
-console.clear
-console.log("VIA CEP")
-let cep = read.question("Digite o CEP: ")
-let urlAPI = `https://viacep.com.br/ws/${cep}/json/`
-// apoio ao entendimento da lógica
-// console.log(urlAPI)
-// uso de promise para recuperar os dados do web service (API)
-fetch(urlAPI)
-    .then((response) => { // para obter os dados
-        return response.json()
-    })
-    .then((dados) => { // para manipular os dados obtidos
-        console.log(dados.logradouro)
-        console.log(dados.bairro)
-        console.log(dados.localidade)
-        console.log(dados.uf)
-    })
-    .catch((error) => {
-        console.log(`Erro ao obter o endereço: ${error}`)
-    })
+    fetch(urlAPI)
+        .then((response) => { 
+            return response.json()
+        })
+        .then((dados) => { 
+            frmFornecedor.inputLogradouro.value = `${dados.logradouro}`
+            frmFornecedor.inputBairro.value = `${dados.bairro}`
+            frmFornecedor.inputCidade.value = `${dados.localidade}`
+            frmFornecedor.uf.value = `${dados.uf}`
+        })
+        .catch((error) => {
+            console.log(`Erro ao obter o endereço: ${error}`)
+        })
+    }
